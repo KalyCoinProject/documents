a8b49d3771862742be2b65a3a4865b03cd219216
AndresCHz45 <camiloandres455@outlook.com>
2022-09-27 16:23:33 -0500

    Documentation Update
    Uploaded docs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# documents
+## Kalychain Docs
+
+[![Github Actions](https://github.com/kalychain-lab/kalychain-docs/actions/workflows/pages.yml/badge.svg)](https://github.com/kalychain-lab/kalychain-docs/actions/workflows/pages.yml)
+
+This is the main repo for contributing to the Kalychain documentation.
+
+If you'd like to contribute, please check out our contribution guide.
+
+For any questions, please contact the Kalychain team.
---
+++ docusaurus.config.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+    title: 'Kalychain',
+    url: 'https://docs.kalychain.io',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/logo.png',
+    organizationName: 'kalychain-lab',
+    projectName: 'kalychain-docs',
+    themes: [
+        [
+          require.resolve("@easyops-cn/docusaurus-search-local"),
+          {
+            hashed: true,
+            language: ["en"],
+          },
+        ],
+    ],
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'dark'
+        },
+        prism: {
+            defaultLanguage: 'go'
+        },
+        navbar: {
+            hideOnScroll: true,
+            title: 'Kalychain',
+            logo: {
+                alt: 'Kalychain Logo',
+                src: 'img/logo.png',
+                href: 'docs/overview'
+            },
+            items: [
+                {
+                    to: 'docs/overview',
+                    activeBasePath: 'docs/',
+                    label: 'Developers',
+                    position: 'left'
+                },
+                {
+                    to: 'docs/tokenomics/overview',
+                    activeBasePath: 'docs/',
+                    label: 'Tokenomics',
+                    position: 'left'
+                },
+                {
+                    href: 'https://github.com/kalychain-lab/kalychain',
+                    position: 'right',
+                    className: 'header-github-link',
+                    'aria-label': 'GitHub repository',
+                },
+            ]
+        },
+        footer: {
+            style: 'dark',
+            copyright: `Made with ❤ by the humans at <a href='https://trapesys.io'>Trapesys</a>`
+        }
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    showLastUpdateAuthor: false,
+                    showLastUpdateTime: false
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css')
+                }
+            }
+        ]
+    ]
+};
